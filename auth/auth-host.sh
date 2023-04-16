d2eafcb7fe5bba0acbf2c194bfe22bbfc9def1a8
Thamtu5012 <128425876+Thamtu5012@users.noreply.github.com>
2023-04-16 19:29:32 +0200

    Update auth-host.sh

--- auth/auth-host.sh
+++ auth/auth-host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $CERTBOT_VALIDATION > ./validation/$CERTBOT_TOKEN
-scp -i ~/.ssh/super_private_key ./validation/$CERTBOT_TOKEN user@hostname:/path/to/.well-known/acme-challenge/
+scp -i ~/.ssh/super_private_key ./validation/$CERTBOT_TOKEN opc@localhost:/path/to/.well-known/acme-challenge/
 ssh -i ~/.ssh/super_private_key user@hostname chmod 664 /path/to/.well-known/acme-challenge/$CERTBOT_TOKEN
