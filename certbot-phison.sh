4e29e9956e88ecb62ee2f5455b44676bf336f8e8
Thamtu5012 <128425876+Thamtu5012@users.noreply.github.com>
2023-04-16 19:23:42 +0200

    Update and rename certbot-superdomain.sh to certbot-phison.sh

--- certbot-phison.sh
+++ certbot-phison.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Generating certificate for superdomain.net..."$'\r\n\r\n'
+echo "Generating certificate for phison.xyz..."$'\r\n\r\n'
 certbot certonly --manual --manual-public-ip-logging-ok --preferred-challenges=http --manual-auth-hook ./auth/auth-host.sh --manual-cleanup-hook ./cleanup/cleanup-host.sh --non-interactive -d superdomain.net -d www.superdomain.net
 read -n1 -r -s -p "Press any key to load certificate...`echo $'\r\n\r\n'`"
-more /etc/letsencrypt/live/superdomain.net/cert.pem
+more /etc/letsencrypt/live/phison.xyz/cert.pem
 read -n1 -r -s -p "Press any key to load private key...`echo $'\r\n\r\n'`"
-more /etc/letsencrypt/live/superdomain.net/privkey.pem
+more /etc/letsencrypt/live/phison.xyz/privkey.pem
 read -n1 -r -s -p "Press any key to end certificate renewal process...`echo $'\r\n\r\n'`"
